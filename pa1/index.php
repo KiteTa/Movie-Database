63ec0c9ee06fc2bb8e9309f941d2c69af07d6940
KiteTa <mjtang@brandeis.edu>
2024-03-20 00:24:09 -0400

    Queries updated

--- pa1/index.php
+++ pa1/index.php
@@ -18,7 +18,7 @@
         <h1 style="text-align:center">COSI 127b</h1><br>
         <h3 style="text-align:center">Connecting Front-End to MySQL DB</h3><br>
     </div>
-    <div class="container">
+    <div id="ageLimit" class="container">
         <form id="ageLimitForm" method="post" action="index.php">
             <div class="input-group mb-3">
                 <input type="text" class="form-control" placeholder="Enter minimum age" name="inputAge" id="inputAge">
@@ -114,7 +114,7 @@
     </div>
     
 
-    <div class="container">
+    <div id="likeMovieFormContainer" class="container">
         <?php
         // MySQL Connection
         $servername = "localhost";
@@ -164,11 +164,13 @@
             <label for='userEmail'>Your Email:</label>
             <input type='email' class='form-control' name='userEmail' id='userEmail' required>
         </div>
-        <button type='submit' class='btn btn-primary' name='likeMovie'>Like Movie</button>
+        <div class="text-right">
+            <button type='submit' class='btn btn-primary' name='likeMovie'>Like Movie</button>
+        </div>
     </form>
-</div>
+    </div>
     
-    <div class="container">
+    <div id="viewOptionsContainer" class="container mb-4">
         <div class="btn-group" role="group" aria-label="View Options">
             <!-- Button to view all movies -->
             <form method="post" action="index.php">
@@ -200,7 +202,7 @@
                 $stmt->execute();
                 $movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                echo "<h2>View All Movies</h2>";
+                echo "<h3>View All Movies</h3>";
                 echo "<table class='table'>";
                 echo "<thead class='bg-dark text-white'><tr><th>ID</th><th>Name</th><th>Genre</th><th>Rating</th><th>Budget</th><th>Production</th></tr></thead>";
                 echo "<tbody>";
@@ -225,7 +227,7 @@
                 $stmt->execute();
                 $actors = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                echo "<h2>View All Actors</h2>";
+                echo "<h3>View All Actors</h3>";
                 echo "<table class='table'>";
                 echo "<thead class='bg-dark text-white'><tr><th>Name</th><th>Gender</th><th>Nationality</th></tr></thead>";
                 echo "<tbody>";
@@ -248,5 +250,305 @@
         ?>
     </div>
 
+    <hr class="round" style="border-top: 5px solid #000; border-radius: 5px;">
+
+    <div id="searchMovie" class="container">
+        <!-- Form to search for a movie -->
+        <form method='post' action='index.php'>
+            <div class='form-group'>
+                <label for='movieName'>Search For A Movie</label>
+                <input type='text' class='form-control' name='movie' id='movieName' required placeholder="Movie name...">
+            </div>
+            <div class="text-right">
+                <button type='submit' class='btn btn-primary' name='searchMovie'>Search</button>
+            </div>
+        </form>
+
+        <?php
+            // MySQL Connection
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "COSI127b";
+
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                if (isset($_POST['searchMovie'])) {
+                    // Get the movie name from the form
+                    $searchMovieName = $_POST['movie'];
+
+                    // Prepare SQL query with parameterized movie name
+                    $stmt = $conn->prepare("SELECT name, rating, production, budget FROM MotionPicture WHERE name = :searchMovieName");
+                    $stmt->bindParam(':searchMovieName', $searchMovieName);
+
+                    // Execute the query
+                    $stmt->execute();
+
+                    // Fetch the results
+                    $movieDetails = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    // Display the results in a table
+                    echo "<h3>Search Results</h3>";
+                    echo "<table class='table'>";
+                    echo "<thead><tr><th>Movie Name</th><th>Rating</th><th>Production</th><th>Budget</th></tr></thead>";
+                    echo "<tbody>";
+                    foreach ($movieDetails as $movie) {
+                        echo "<tr>";
+                        echo "<td>{$movie['name']}</td>";
+                        echo "<td>{$movie['rating']}</td>";
+                        echo "<td>{$movie['production']}</td>";
+                        echo "<td>{$movie['budget']}</td>";
+                        echo "</tr>";
+                    }
+                    echo "</tbody>";
+                    echo "</table>";
+                }
+            } catch(PDOException $e) {
+                echo "Error: " . $e->getMessage();
+            }
+            $conn = null;
+            ?>
+    </div>
+
+    <hr class="round" style="border-top: 5px solid #000; border-radius: 5px;">
+
+    <div id="searchLiked" class="container">
+        <!-- Form to search for a movie -->
+        <form method='post' action='index.php'>
+            <div class='form-group'>
+                <label for='userEmail'>Search for liked movies</label>
+                <input type='email' class='form-control' name='userEmail' id='userEmail' required placeholder="Email address...">
+            </div>
+            <div class="text-right">
+                <button type='submit' class='btn btn-primary' name='searchLiked'>Search</button>
+            </div>
+        </form>
+
+        <?php
+        // MySQL Connection
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "COSI127b";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            if (isset($_POST['searchLiked'])) {
+                // Get the user's email from the form
+                $userEmail = $_POST['userEmail'];
+
+                // Prepare SQL query with parameterized user's email
+                $stmt = $conn->prepare("SELECT name FROM User WHERE email = :userEmail");
+                $stmt->bindParam(':userEmail', $userEmail);
+                $stmt->execute();
+                $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+                
+
+                //Check if user exists in the system
+                if ($userData) {
+                    $username = $userData['name'];
+                    $stmt = $conn->prepare("SELECT MotionPicture.name, MotionPicture.rating, MotionPicture.production, MotionPicture.budget FROM MotionPicture INNER JOIN Likes ON MotionPicture.id = Likes.mpid WHERE Likes.uemail = :userEmail");
+                    $stmt->bindParam(':userEmail', $userEmail);
+                    $stmt->execute();
+                    $likedMovies = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    // Execute the query
+                    $stmt->execute();
+
+                    // Fetch the results
+                    $likedMovies = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    if ($likedMovies) {
+                        echo "<h3>{$username}'s Liked Movies</h3>";
+                        echo "<table class='table'>";
+                        echo "<thead><tr><th>Movie Name</th><th>Rating</th><th>Production</th><th>Budget</th></tr></thead>";
+                        echo "<tbody>";
+                        foreach ($likedMovies as $movie) {
+                            echo "<tr>";
+                            echo "<td>{$movie['name']}</td>";
+                            echo "<td>{$movie['rating']}</td>";
+                            echo "<td>{$movie['production']}</td>";
+                            echo "<td>{$movie['budget']}</td>";
+                            echo "</tr>";
+                        }
+                        echo "</tbody>";
+                        echo "</table>";
+                    } else {
+                        echo "<p>You haven't liked a movie yet.</p>";
+                    }
+                } else {
+                    echo "<p>User not found &#128542;. Please check your email address. </p>";
+                }
+            }
+        } catch(PDOException $e) {
+                echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        ?>
+    </div> 
+
+    <hr class="round" style="border-top: 5px solid #000; border-radius: 5px;">
+
+    <div id="searchByCountry" class="container">
+        <!-- Form to search for a movie -->
+        <form method='post' action='index.php'>
+            <div class='form-group'>
+                <label for='country'>Search Movies By Shooting Country</label>
+                <input type='country' class='form-control' name='country' id='country' required placeholder="Where...">
+            </div>
+            <div class="text-right">
+                <button type='submit' class='btn btn-primary' name='searchByCountry'>Search</button>
+            </div>
+        </form>
+
+        <?php
+        // MySQL Connection
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "COSI127b";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            if (isset($_POST['searchByCountry'])) {
+                // Get the user's email from the form
+                $countryName = $_POST['country'];
+
+                // Prepare SQL query with parameterized user's email
+                $stmt = $conn->prepare("SELECT country FROM Location WHERE country = :countryName");
+                $stmt->bindParam(':countryName', $countryName);
+                $stmt->execute();
+                $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+                
+
+                //Check if location exists in the system
+                if ($userData) {
+                    $stmt = $conn->prepare("SELECT DISTINCT MotionPicture.name FROM MotionPicture INNER JOIN Location ON MotionPicture.id = Location.mpid WHERE Location.country = :countryName");
+                    $stmt->bindParam(':countryName', $countryName);
+                    $stmt->execute();
+                    $likedMovies = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    // Execute the query
+                    $stmt->execute();
+
+                    // Fetch the results
+                    $locations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    if ($locations) {
+                        echo "<h3>Movies that are filmed at {$countryName}</h3>";
+                        echo "<table class='table'>";
+                        echo "<thead><tr><th>Movie Name</th></tr></thead>";
+                        echo "<tbody>";
+                        foreach ($locations as $movie) {
+                            echo "<tr>";
+                            echo "<td>{$movie['name']}</td>";
+                            echo "</tr>";
+                        }
+                        echo "</tbody>";
+                        echo "</table>";
+                    } else {
+                        echo "<p>Nothing has been filmed here before.</p>";
+                    }
+                } else {
+                    echo "<p>We don't have record for this location yet. But wait for it...&#x2708;&#x1F378;</p>";
+                }
+            }
+        } catch(PDOException $e) {
+                echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        ?>
+    </div> 
+
+    <hr class="round" style="border-top: 5px solid #000; border-radius: 5px;">
+
+    <div id="zipCodeDirector" class="container">
+        <!-- Form to search for a movie -->
+        <form method='post' action='index.php'>
+            <div class='form-group'>
+                <label for='zip'>Search For TV Directors By Zip Code</label>
+                <input type='text' class='form-control' name='zip' id='zip' required placeholder="Zip Code Number...">
+            </div>
+            <div class="text-right">
+                <button type='submit' class='btn btn-primary' name='zipCodeDirector'>Search</button>
+            </div>
+        </form>
+
+        <?php
+        // MySQL Connection
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "COSI127b";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            if (isset($_POST['zipCodeDirector'])) {
+                $zipCodeInput = $_POST['zip']; // Store user input zip code
+            
+                $stmt = $conn->prepare("SELECT zip FROM Location WHERE zip = :zipCode");
+                $stmt->bindParam(':zipCode', $zipCodeInput);
+                $stmt->execute();
+                $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+                //Check if zip code exists in the system
+                if ($userData) {
+                    $stmt = $conn->prepare("SELECT DISTINCT People.name AS director_name, TVSeries.name AS series_name 
+                        FROM (
+                            SELECT *
+                            FROM Role
+                            WHERE Role.role_name = 'Director'
+                        ) AS DirectorRoles
+                        INNER JOIN People ON DirectorRoles.pid = People.id
+                        INNER JOIN (
+                            SELECT MotionPicture.id, MotionPicture.name
+                            FROM MotionPicture
+                            INNER JOIN Series ON MotionPicture.id = Series.mpid
+                            INNER JOIN Location ON MotionPicture.id = Location.mpid
+                            WHERE Location.zip = :zipCode
+                        ) AS TVSeries ON DirectorRoles.mpid = TVSeries.id
+                        ");
+                    $stmt->bindParam(':zipCode', $zipCodeInput);
+                    $stmt->execute();
+                    $locations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            
+                    // Output the results
+                    if ($locations) {
+                        echo "<h3>Directors had worked at $zipCodeInput</h3>";
+                        echo "<table class='table'>";
+                        echo "<thead><tr><th>Director Name</th><th>TV Name</th></tr></thead>";
+                        echo "<tbody>";
+                        foreach ($locations as $movie) {
+                            echo "<tr>";
+                            echo "<td>{$movie['director_name']}</td>"; 
+                            echo "<td>{$movie['series_name']}</td>";
+                            echo "</tr>";
+                        }
+                        echo "</tbody>";
+                        echo "</table>";
+                    } else {
+                        echo "<p>No one had worked here yet.</p>";
+                    }
+                } else {
+                    echo "<p>We don't have record for this zip code yet. But wait for it...&#x2708;&#x1F378;</p>";
+                }
+            }
+        } catch(PDOException $e) {
+                echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        ?>
+    </div> 
+
+    <hr class="round" style="border-top: 5px solid #000; border-radius: 5px;">
+    
 </body>
 </html>
